bc46a6e6a944d8016b7ffca2548d4a2c85ddf3eb
jjetzinger <71641667+jjetzinger@users.noreply.github.com>
2021-06-07 14:52:12 +0200

    RoundAdapter finished
    Co-Authored-By: eliasongun <68249881+eliasongun@users.noreply.github.com>
    Co-Authored-By: fschaur18 <71641609+fschaur18@users.noreply.github.com>
    Co-Authored-By: lgittmaier18 <71641551+lgittmaier18@users.noreply.github.com>

--- app/src/main/java/htlgkr/scorebook/RoundAdapter.java
+++ app/src/main/java/htlgkr/scorebook/RoundAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.text.ParseException;
 import java.util.List;
@@ -12,6 +13,9 @@ public class RoundAdapter extends BaseAdapter {
 
     public List<Round> rounds;
     LayoutInflater layoutInflater;
+    TextView nameTextView;
+    TextView dateTextView;
+    TextView scoreTextView;
 
     public RoundAdapter(List<Round> rounds, LayoutInflater layoutInflater) {
         this.rounds = rounds;
@@ -20,53 +24,40 @@ public class RoundAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return rounds.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return rounds.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return 0L;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Round note = notes.get(position);     //current Note
+        Round round = rounds.get(position);     //current Round
 
-        // due date of note
-        try {
-            dueDate.setTime(sdf.parse(note.getDateTime()));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
 
         View view;
         if (convertView == null) {
             view = layoutInflater.inflate(R.layout.listview_object, null);
-
         } else {
             view = convertView;
         }
 
         // sets the fields in the listview
-        titleTextView = view.findViewById(R.id.titleField);
-        dateTextView = view.findViewById(R.id.dateField);
+        nameTextView = view.findViewById(R.id.nameTextView);
+        dateTextView = view.findViewById(R.id.dateTextView);
+        scoreTextView = view.findViewById(R.id.scoreTextView);
 
-        titleTextView.setText(note.getTitle());
-        dateTextView.setText(note.getDateTime());
+        nameTextView.setText(round.getName());
+        dateTextView.setText(round.getDate());
+        scoreTextView.setText(round.getScore());
 
-        // if note is due -> set background red
-        if (dueDate.getTime().getTime() < now.getTime()) {
-            titleTextView.setBackgroundResource(R.color.lightred);
-            dateTextView.setBackgroundResource(R.color.lightred);
-        } else {
-            titleTextView.setBackgroundResource(R.color.lightgrey);
-            dateTextView.setBackgroundResource(R.color.lightgrey);
-        }
 
         return view;
     }
